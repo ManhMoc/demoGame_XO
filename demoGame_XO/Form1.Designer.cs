c9ce1fee26ea115096de0e2660ccb2a153b8c1ad
ManhMoc <38889338+ManhMoc@users.noreply.github.com>
2018-12-25 10:34:48 +0700

    Initial commit

---
+++ demoGame_XO/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace demoGame_XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHang = new System.Windows.Forms.TextBox();
+            this.txtCot = new System.Windows.Forms.TextBox();
+            this.btnTao = new System.Windows.Forms.Button();
+            this.grb = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Mistral", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập số hàng";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Mistral", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(281, 13);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập số cột";
+            // 
+            // txtHang
+            // 
+            this.txtHang.Location = new System.Drawing.Point(139, 13);
+            this.txtHang.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtHang.Name = "txtHang";
+            this.txtHang.Size = new System.Drawing.Size(116, 21);
+            this.txtHang.TabIndex = 2;
+            // 
+            // txtCot
+            // 
+            this.txtCot.Location = new System.Drawing.Point(399, 15);
+            this.txtCot.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtCot.Name = "txtCot";
+            this.txtCot.Size = new System.Drawing.Size(122, 21);
+            this.txtCot.TabIndex = 3;
+            // 
+            // btnTao
+            // 
+            this.btnTao.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTao.Font = new System.Drawing.Font("Mistral", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTao.Location = new System.Drawing.Point(187, 46);
+            this.btnTao.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnTao.Name = "btnTao";
+            this.btnTao.Size = new System.Drawing.Size(162, 23);
+            this.btnTao.TabIndex = 5;
+            this.btnTao.Text = "Tạo ";
+            this.btnTao.UseVisualStyleBackColor = false;
+            this.btnTao.Click += new System.EventHandler(this.btnTao_Click);
+            // 
+            // grb
+            // 
+            this.grb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grb.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.grb.Font = new System.Drawing.Font("Monotype Corsiva", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grb.Location = new System.Drawing.Point(3, 75);
+            this.grb.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.grb.Name = "grb";
+            this.grb.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.grb.Size = new System.Drawing.Size(559, 404);
+            this.grb.TabIndex = 6;
+            this.grb.TabStop = false;
+            this.grb.Text = "Cờ Cá Rô";
+            this.grb.Enter += new System.EventHandler(this.grb_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 491);
+            this.Controls.Add(this.grb);
+            this.Controls.Add(this.btnTao);
+            this.Controls.Add(this.txtCot);
+            this.Controls.Add(this.txtHang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Mistral", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "O_X_O_X";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHang;
+        private System.Windows.Forms.TextBox txtCot;
+        private System.Windows.Forms.Button btnTao;
+        private System.Windows.Forms.GroupBox grb;
+    }
+}
+
---
+++ demoGame_XO/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demoGame_XO
+{
+    
+    public partial class Form1 : Form
+    {
+        public static char[,] arr;
+        public static char last = 'O';
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void btnTao_Click(object sender, EventArgs e)
+        {
+            
+            int row = int.Parse(txtHang.Text);
+            int colum = int.Parse(txtCot.Text);
+            Button[,] btn = new Button[row,colum];
+           
+            arr = new char[row, colum];
+            for (int i=0;i<row;i++)
+                for(int j=0;j<colum;j++)
+                {
+                    btn[i, j] = new Button();
+                    btn[i,j].Width  = 50;
+                    btn[i, j].Height = 50;
+                    btn[i,j].Location = new Point(j* 50, i * 50);
+                    btn[i,j].Text = "";
+                    btn[i,j].Click += new EventHandler(button_click);
+                    btn[i,j].ForeColor = Color.Blue;
+                    arr[i, j] = ' ';
+                    grb.Controls.Add(btn[i,j]);
+                }
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            // MessageBox.Show(((Button)sender).Location.ToString());
+            //MessageBox.Show(((Button)sender).Top.ToString());
+            //MessageBox.Show(((Button)sender).Left.ToString());
+            int x = ((Button)sender).Left / 50;
+            int y = ((Button)sender).Top / 50;
+           // MessageBox.Show("[" + x + "," + y + "]");
+            if (last == 'O')
+            {
+                ((Button)sender).Text = "X";
+                
+                last = 'X';
+                arr[x, y] = 'X';
+                if (IsWinRow(x, y)) MessageBox.Show("X win");
+                if (IsWinColumn(x, y)) MessageBox.Show("X win");
+                if (IsWinDiagonal1(x, y)) MessageBox.Show("X win");
+                if (IsWinDiagonal2(x, y)) MessageBox.Show("X win");
+                /* foreach(Button btn in grb.Controls)
+                 {
+                     if (btn.Location == ((Button)sender).Location)
+                     {
+                         int x = ((Button)sender).Left/50;
+                         int y = ((Button)sender).Top / 50;
+                         MessageBox.Show("["+x+","+y+"]");
+                     }        
+                 }*/
+            }
+            else
+            {
+                ((Button)sender).Text = "O";
+                arr[x, y] = 'O';
+                if (IsWinRow(x, y)) MessageBox.Show("O win");
+                if (IsWinColumn(x, y)) MessageBox.Show("O win");
+                if (IsWinDiagonal1(x, y)) MessageBox.Show("O win");
+                if (IsWinDiagonal2(x, y)) MessageBox.Show("O win");
+                last = 'O';
+            }
+        }
+        public bool IsWinRow(int x,int y)//kiem tra hang.
+        {
+            char c = arr[x, y];
+            int count = 0;
+            int k = x;
+            int kmax = x;
+            int kmin = x;
+            int max = int.Parse(txtCot.Text)-1;
+            while(k>=0 && k <= max)
+            {
+                if ( k>=x && arr[k, y] == c)
+                {
+                    count++;
+                    kmax = k;
+                    if (k == max) k = x - count;
+                    else
+                    k++;
+                    
+                }
+                else 
+                {
+                    
+                    k = x - count;
+                    kmin = k;
+                    if (k>=0 && arr[k, y] == c)
+                    {
+                        if (arr[k + 1, y] != c) break;//Node truoc !=c;
+                        count++;
+                    }
+                    else break;
+                }
+            }           
+            if (kmin < 0) kmin = 0;
+            if (kmax < max) kmax++;
+           // if (kmin > 0) kmin--;
+            bool bl = arr[kmin, y] == ' ' || arr[kmax, y] == ' '
+                || arr[kmax,y]==c || arr[kmin,y]==c;
+            if (bl && count >= 5) return true;
+
+            
+
+            return false;
+        }
+        public bool IsWinColumn(int x,int y)//Kiem tra cot
+        {
+            char c = arr[x, y];
+            int count = 0;
+            int k = y;
+            int kmax = y;
+            int kmin = y;
+            int max = int.Parse(txtHang.Text) - 1;
+            while (k >= 0 && k <= max)
+            {
+                if (k >= y && arr[x, k] == c)
+                {
+                    count++;
+                    kmax = k;
+                    if (k == max) k = y - count;
+                    else
+                        k++;
+                }
+                else
+                {
+                    k = y - count;
+                    kmin = k;
+                    if (k >= 0 && arr[x, k] == c)
+                    {
+                        if (arr[x, k+1] != c) break;
+                        count++;
+                    }
+                    else break;
+                }
+            }
+            if (kmin < 0) kmin = 0;
+            if (kmax < max) kmax++;
+            bool bl = arr[x, kmin] == ' ' || arr[x, kmax] == ' '
+                || arr[x, kmax] == c || arr[x, kmin] == c;
+            if (bl && count >= 5) return true;
+            return false;
+        }
+        public bool IsWinDiagonal1(int x,int y)//Theo chieu tang;
+        {
+            char c = arr[x, y];
+            int count = 0;
+            int kx = x;
+            int ky = y;
+            int kmax_y = y;
+            int kmin_y = y;
+            int kmax_x = x;
+            int kmin_x = x;
+            int maxx = int.Parse(txtCot.Text) - 1;
+            int maxy = int.Parse(txtHang.Text) - 1;
+            while( kx >= 0 && kx <= maxx
+                && ky >= 0 && ky <= maxy )
+            {
+                if (ky >= y && arr[kx, ky] == c
+                    && kx >= x && arr[kx, ky] == c)
+                {
+                    count++;
+                    kmax_x = kx;
+                    kmax_y = ky;
+                    if (kx == maxx || ky == maxy)
+                    {
+                        kx = x - count;
+                        ky = y - count;
+                    }
+                    else
+                    {
+                        kx++;
+                        ky++;
+                    };
+                }
+                else
+                {
+                    ky = y - count;
+                    kx = x - count;
+                    kmin_x = kx;
+                    kmin_y = ky;
+                    if (kx >= 0 && ky>=0 && arr[kx, ky] == c)
+                    {
+                        if (arr[kx+1 , ky+1] != c) break;
+                        count++;
+                    }
+                    else break;
+                }
+            }
+            if (kmin_x < 0 || kmin_y < 0)
+            {
+                kmin_x++;
+                kmin_y++;
+            }
+            if (kmax_x < maxx && kmax_y < maxy)
+            {
+                kmax_x++;
+                kmax_y++;
+            }
+            bool bl = arr[kmin_x, kmin_y] == ' ' || arr[kmax_x, kmax_y] == ' '
+                || arr[kmax_x, kmax_y] == c || arr[kmin_x, kmin_y] == c;
+            if (bl && count >= 5) return true;
+           
+            return false;
+        }
+       
+        public bool IsWinDiagonal2(int x, int y)//Theo chieu giam;
+        {
+            char c = arr[x, y];
+            int count = 0;
+            int kx = x;
+            int ky = y;
+            int kmax_y = y;
+            int kmin_y = y;
+            int kmax_x = x;
+            int kmin_x = x;
+            int maxx = int.Parse(txtCot.Text) - 1;
+            int maxy = int.Parse(txtHang.Text) - 1;
+            while (kx >= 0 && kx <= maxx
+                && ky >= 0 && ky <= maxy)
+            {
+                if (kx >= x && arr[kx, ky] == c
+                    && ky >= 0 && arr[kx, ky] == c)//
+                {
+                    count++;
+                    kmax_x = kx;
+                    kmax_y = ky;
+                    if (ky == 0)
+                    {
+                        kx = x - count;
+                        ky = y - count;
+                    }
+                    else
+                    {
+                        kx++;
+                        ky--;
+                    };
+                }
+                else
+                {
+                    ky = y + count;
+                    kx = x - count;
+                    kmin_x = kx;
+                    kmin_y = ky;
+                    if (kx >= 0 && ky <=maxy && arr[kx, ky] == c)
+                    {
+                        if (arr[kx + 1, ky - 1] != c) break;
+                        count++;
+                    }
+                    else break;
+                }
+            }
+            if (kmin_x < 0)
+            {
+                kmin_x++;
+                kmin_y--;
+            }
+            if (kmax_y<0)
+            {
+                kmax_x--;
+                kmax_y++;
+            }
+            bool bl = arr[kmin_x, kmin_y] == ' ' || arr[kmax_x, kmax_y] == ' '
+                || arr[kmax_x, kmax_y] == c || arr[kmin_x, kmin_y] == c;
+            if (bl && count >= 5) return true;
+
+            return false;
+        }
+        private void grb_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
